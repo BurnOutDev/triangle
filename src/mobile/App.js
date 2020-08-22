a081c1e26812572d46cb9523b4e641b1bfb0a195
Irakli Murusidze <IrakliMurusidze@live.com>
2020-08-22 16:59:40 +0400

    configured eslint

--- src/mobile/App.js
+++ src/mobile/App.js
@@ -1,40 +1,22 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
-import { mapping, light as lightTheme } from '@eva-design/eva';
-import { EvaIconsPack } from '@ui-kitten/eva-icons';
-import LogIn from './screens/Authentication/LogIn';
+import {StatusBar} from 'react-native';
+import {ApplicationProvider, IconRegistry} from '@ui-kitten/components';
+import {mapping, light as lightTheme} from '@eva-design/eva';
+import {EvaIconsPack} from '@ui-kitten/eva-icons';
 import AppNavigator from './screens/AppNavigator';
-import Authentication from './screens/Authentication/Authentication';
-import SignUp from './screens/Authentication/SignUp';
-import IntroLayout from './screens/Intro/IntroLayout';
-import Explore from './screens/Home/Explore';
-import { bgMagenta } from 'chalk'
-import AsyncStorage from '@react-native-community/async-storage';
 
 const App: () => React$Node = () => (
   <>
-    <StatusBar backgroundColor='transparent' translucent barStyle='dark-content' />
+    <StatusBar
+      backgroundColor="transparent"
+      translucent
+      barStyle="dark-content"
+    />
     <IconRegistry icons={EvaIconsPack} />
     <ApplicationProvider mapping={mapping} theme={lightTheme}>
       <AppNavigator />
     </ApplicationProvider>
   </>
-)
+);
 
 export default App;
--- src/mobile/axios.js
+++ src/mobile/axios.js
@@ -1,48 +1,46 @@
 import axios from 'axios';
-import { DevSettings } from 'react-native';
-import MockAdapter from 'axios-mock-adapter'
-import AsyncStorage from '@react-native-community/async-storage'
-import { axiosLog, ip } from './config.json'
-import { logger } from 'react-native-logs'
-import { colorConsoleSync } from 'react-native-logs/dist/transports/colorConsoleSync';
-import log from './log'
+import {DevSettings} from 'react-native';
+import MockAdapter from 'axios-mock-adapter';
+import AsyncStorage from '@react-native-community/async-storage';
+import {ip} from './config.json';
+import log from './log';
 
-axios.interceptors.request.use(async (config) => {  
-    const token = await AsyncStorage.getItem('token')
+axios.interceptors.request.use(
+  async config => {
+    const token = await AsyncStorage.getItem('token');
 
     config.headers = {
-        Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json'
-    }
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    };
 
-    config.baseURL = `http://${ip}:5001/`
+    config.baseURL = `http://${ip}:5001/`;
 
-    log.apiRequest(config.url)
+    log.apiRequest(config.url);
 
     return config;
-}, error => {
-
+  },
+  error => {
     return Promise.reject(error);
-});
+  },
+);
 
-axios.interceptors.response.use((response) => {
+axios.interceptors.response.use(
+  response => {
     return response;
-}, error => {
+  },
+  error => {
     if (error ?? error.response ?? error.response.status === 401) {
-        AsyncStorage.removeItem('token').then(DevSettings.reload())
+      AsyncStorage.removeItem('token').then(DevSettings.reload());
     }
 
-    log.error(error)
+    log.error(error);
 
     return Promise.reject(error);
-});
+  },
+);
 
-const logRequest = ({ url }) => {
-    !fs.existsSync(`./logs/${axiosLog}`) && fs.writeFileSync(`./logs/${axiosLog}`)
-    fs.appendFileSync(`./logs/${axiosLog}`, `Url: ${url} Date: ${moment().format('DD/MM/YYYY - HH:mm')}\n`)
-}
-
-const mock = new MockAdapter(axios, { onNoMatch: 'passthrough' })
+const mock = new MockAdapter(axios, {onNoMatch: 'passthrough'});
 
 // mock.onGet('/Restaurant/Cuisines').reply(200, require('./Mock/Get/Restaurant/Cuisines.json'))
 // mock.onPost('/Restaurant/RestaurantsPerCategory').reply(200, require('./Mock/Get/Restaurant/Restaurants.json'))
@@ -50,4 +48,4 @@ const mock = new MockAdapter(axios, { onNoMatch: 'passthrough' })
 // mock.onGet('/Restaurant/Restaurant/1').reply(200, require('./Mock/Get/Restaurant/Restaurant.json'))
 // mock.onGet('/Menu/GetMenuItems/1').reply(200, require('./Mock/Get/Restauarant/MenuItems.json'))
 
-export default axios
+export default axios;
